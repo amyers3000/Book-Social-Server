c6b951d81081bfb23452420af17c374c8dc93a4c
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-09-30 09:18:51 -0500

    added create user and get all user logic

--- controllers/favorite.js
+++ controllers/favorite.js
@@ -2,8 +2,18 @@ const db = require('../models/index')
 const { Favorite, Book } = db
 
 async function showAllFavorites(req, res){
-    
+    try {
+        const foundFavorites = await Book.findAll()
+        if(foundFavorites){
+            res.json(foundFavorites)
+        }else{
+            res.status(400).json({message: "No books saved"})
+        }
+    } catch (error) {
+        console.log(error)
+    }
 }
+
 function showFavorite(req, res){
 
 }
