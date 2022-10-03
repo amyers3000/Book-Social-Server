da7c3d21ebaefb7dfd72f7204ad57bd164e924e6
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-10-03 09:45:56 -0500

    comment controller logic completed

--- controllers/comment.js
+++ controllers/comment.js
@@ -5,51 +5,96 @@ const { Book, Comment } = db
 async function addComment(req, res){
     let { bookId } = req.params
 
-    let book = await Book.findOne({
-        where: { bookId: bookId }
-    })
-
-    if(!book){
-        res.status(404).json({message: `Cound not find book wiht id "${bookId}"`})
+    try {
+        let book = await Book.findOne({
+            where: { bookId: bookId }
+        })
+    
+        if(!book){
+            res.status(404).json({message: `Cound not find book wiht id "${bookId}"`})
+        }
+    
+        const comment = await Comment.create({
+            ...req.body,
+            userId: req.user.userId,
+            bookId
+        })
+        if(comment){
+        res.json({message: "comment saved"})
+        }else{
+            res.status(404).json({message: "comment was not made"})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
     }
-
-    const comment = await Comment.create({
-        ...req.body,
-        userId: req.user.userId,
-        bookId
-    })
-
-    res.json({message: "comment saved"})
 }
 
-function editComment(req, res){
+async function editComment(req, res){
+    let { commentId } = req.params
 
+    try {
+        const comment = await Comment.findOne({
+            where:{
+                commentId: commentId,
+            }
+        })
+    
+        if(!comment) {
+            res.status(404).json({message: "Could not find comment associated with book"})
+        }else if(comment.userId !== req.user?.userId){
+            res.status(403).json({message: "You do not have permission to edit comment"})
+        }else{
+            await comment.set({
+                ...req.body
+            }).save()
+            res.json({message: "Comment updated"})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
-function showAllComments(req, res){
+async function showComment(req, res){
+    let { commentId } = req.params
 
+    try {
+        const comment = await Comment.findOne({
+            where:{
+                commentId: commentId,
+            }
+        })
+        if(!comment) {
+            res.status(404).json({message: "Could not find comment associated with book"})
+        }else{
+            res.json(comment)
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 }
 
 async function deleteComment(req, res){
-    let { bookId } = req.params
     let { commentId } = req.params
 
-    const comment = await Comment.findOne({
-        where:{
-            commentId: commentId,
-            bookId: bookId
+    try {
+        const comment = await Comment.findOne({
+            where:{
+                commentId: commentId,
+            }
+        })
+    
+        if(!comment) {
+            res.status(404).json({message: "Could not find comment associated with book"})
+        }else if(comment.userId !== req.user?.userId){
+            res.status(403).json({message: "You do not have permission to delete comment"})
+        }else {
+            await comment.destroy()
+            res.json({message: "comment deleted"})
         }
-    })
-
-    if(!comment) {
-        res.status(404).json({message: "Could not find comment associated with book"})
-    }else if(comment.userId !== req.user?.userId){
-        res.status(403).json({message: "You do not have permission to delete comment"})
-    }else {
-        await comment.destroy()
-        res.json({message: "comment deleted"})
+    } catch (error) {
+        res.status(500).json({ message: error })
     }
 }
 
 
-module.exports = { editComment, addComment, deleteComment }
+module.exports = { editComment, addComment, deleteComment, showComment }
--- routes/comment.js
+++ routes/comment.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
-const { editComment, addComment, deleteComment } = require('../controllers/comment')
+const { editComment, addComment, deleteComment, showComment } = require('../controllers/comment')
 const { validateJWT } = require('../middleware/auth')
 
-router.post('/:bookId/comment', validateJWT, addComment)
-router.delete('/:bookId/:commentId', validateJWT, deleteComment)
-router.put('/:bookId/:commentId', editComment)
+router.get('/:commentId', validateJWT, showComment)
+router.post('/:bookId', validateJWT, addComment)
+router.delete('/:commentId', validateJWT, deleteComment)
+router.put('/:commentId', validateJWT, editComment)
+
 
 
 
