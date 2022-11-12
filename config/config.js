b947ba6259b0524e6ae3b6bf0cdd7e80325405cb
Alex Myers <amyers3000@Alexs-MBP.attlocal.net>
2022-11-12 09:28:25 -0600

    modified some things

--- config/config.js
+++ config/config.js
@@ -2,9 +2,9 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DATABASE,
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
@@ -16,10 +16,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "username": process.env.RDS_USERNAME,
+    "password": process.env.RDS_PASSWORD,
+    "database": process.env.RDS_DATABASE,
+    "host": process.env.RDS_HOSTNAME,
+    "port": process.env.RDS_PORT,
     "dialect": "postgres"
   }
 }
